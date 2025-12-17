9ef4c2c7b45ce41ae296bc09912aa6f078f83fd0
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-12-17 20:03:22 +0530

    Change server port from 8080 to 5000

---
+++ main1.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import metric_runner
+
+app = Flask(__name__)
+
+@app.route("/")
+def run_queries_and_publish_metrics():
+    project_id = "ajith-8006"
+
+    queries = metric_runner.load_queries("queries.yaml")
+    results = []
+
+    for query_data in queries:
+        result = metric_runner.run_query_and_publish_metric(
+            query_name=query_data["name"],
+            metric_type=query_data["metric_type"],
+            query=query_data["query"],
+            project_id=project_id   # ✅ FIXED
+        )
+        results.append(result)
+
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
